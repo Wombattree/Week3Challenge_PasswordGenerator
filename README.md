f6784d16640998586aba0420d19fe859b9315743
Wombattree <63911123+Wombattree@users.noreply.github.com>
2022-06-21 20:20:13 +0930

    Updated JS code to better fit brief, filled out ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Week3Challenge_PasswordGenerator
+# Week 3 Challenge - Password Generator
  
+# What Is This?
+A task for the Adelaide Uni Bootcamp to create a simple password generator with the option to chose the password length and what characters are valid for the password.
+
+# What Did I Do?
+* Wrote the JS code to randomly generate a password
+
+# Where Can I See It?
+[Click here for to see the website live](https://wombattree.github.io/Week3Challenge_PasswordGenerator/)
+
+![Preview Image](./assets/readMeImage.png?raw=true "Preview Image")
--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 var generateButton = document.getElementById("generatePasswordButton");
 var passwordText = document.getElementById("passwordDisplay");
 
-var passwordLengthSlider = document.getElementById("passwordLengthSlider");
-var isLowerCaseValidButton = document.getElementById("isLowerCaseValid");
-var isUpperCaseVaidButton = document.getElementById("isUpperCaseValid");
-var areNumbersValidButton = document.getElementById("areNumbersValid");
-var areSpecialValidButton = document.getElementById("areSpecialValid");
-
 var lowerCaseCharcters = GetLowerCaseCharacters();
 var upperCaseCharacters = GetUpperCaseCharacters();
 var numberCharacters = GetNumberCharacters();
@@ -16,26 +10,32 @@ function GeneratePassword()
 {
   var validCharacters = [];
 
-  if (isLowerCaseValidButton.checked) {for (const element of lowerCaseCharcters) validCharacters.push(element);}
-  if (isUpperCaseVaidButton.checked) {for (const element of upperCaseCharacters) validCharacters.push(element);}
-  if (areNumbersValidButton.checked) {for (const element of numberCharacters) validCharacters.push(element);}
-  if (areSpecialValidButton.checked) {for (const element of specialCharacters) validCharacters.push(element);}
+  var passwordLength = prompt("Please enter the desired password length\nMinimum 8, maximum 128")
 
-  if (validCharacters.length > 0)
+  if (passwordLength < 8) { PasswordIsTheWrongSize(true); return null; }
+  else if (passwordLength > 128) { PasswordIsTheWrongSize(false); return null; }
+
+  var areLowerCaseValid = confirm("Should lower case letters be allowed in the password");
+  var areUpperCaseValid = confirm("Should upper case letters be allowed in the password");
+  var areNumberValid = confirm("Should numbers be allowed in the password");
+  var areSpecialValid = confirm("Should special characters be allowed in the password");
+
+  if (areLowerCaseValid) {for (const element of lowerCaseCharcters) validCharacters.push(element);}
+  if (areUpperCaseValid) {for (const element of upperCaseCharacters) validCharacters.push(element);}
+  if (areNumberValid) {for (const element of numberCharacters) validCharacters.push(element);}
+  if (areSpecialValid) {for (const element of specialCharacters) validCharacters.push(element);}
+
+  if (validCharacters.length <= 0) { NoValidCharacters(); return null; }
+  else
   {
     var password = "";
-  
-    for (let i = 0; i < passwordLengthSlider.value; i++) 
+
+    for (let i = 0; i < passwordLength; i++) 
     {
       password += GetCharacter(validCharacters);
     }
   
     return password;
-  }
-  else
-  {
-    NoValidCharacters();
-    return null;
   }
 }
 
@@ -69,7 +69,13 @@ function GetSpecialCharacters()
 
 function NoValidCharacters()
 {
-  console.log("You must enable at least one option");
+  alert("You must enable at least one option");
+}
+
+function PasswordIsTheWrongSize(tooSmall)
+{
+  var size = tooSmall ? "small" : "large";
+  alert("The number you have entered was too " + size + ".\nThe password should be within 8 and 128 characters.");
 }
 
 function WritePassword()

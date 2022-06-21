0fe7c11e24873906cb0e337fe7fde8021470b914
Wombattree <63911123+Wombattree@users.noreply.github.com>
2022-06-22 00:50:18 +0930

    Merged down variables, sanitised inputs and added comments

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 var generateButton = document.getElementById("generatePasswordButton");
 var passwordText = document.getElementById("passwordDisplay");
 
-var lowerCaseCharcters = GetAlphabeticalCharacters(true);
-var upperCaseCharacters = GetAlphabeticalCharacters(false);
-var numberCharacters = GetNumericalCharacters();
-var specialCharacters = GetSpecialCharacters();
+var minCharacters = 8;
+var maxCharacters = 128;
 
 function GeneratePassword()
 {
   var validCharacters = [];
 
-  var passwordLength = prompt("Please enter the desired password length\nMinimum 8, maximum 128")
+  var passwordLength = prompt("Please enter the desired password length\nMinimum " + minCharacters + ", maximum " + maxCharacters + ".")
 
-  if (passwordLength < 8) { PasswordIsTheWrongSize(true); return null; }
-  else if (passwordLength > 128) { PasswordIsTheWrongSize(false); return null; }
+  //Sanitise password length
+  if (isNaN(passwordLength)) { PasswordIsNotANumber(); return null; }
+  else if (passwordLength < minCharacters) { PasswordIsTheWrongSize(true); return null; }
+  else if (passwordLength > maxCharacters) { PasswordIsTheWrongSize(false); return null; }
 
-  var areLowerCaseValid = confirm("Should lower case letters be allowed in the password");
-  var areUpperCaseValid = confirm("Should upper case letters be allowed in the password");
-  var areNumberValid = confirm("Should numbers be allowed in the password");
-  var areSpecialValid = confirm("Should special characters be allowed in the password");
+  //Ask for permission to use each type of charcter, then if permission is granted get all of that type of charcter and add it to the array of valid characters
+  if (ConfirmationMessage("lower case letters"))
+    GetAlphabeticalCharacters(true).forEach(element => { validCharacters.push(element) });
+  if (ConfirmationMessage("upper case letters")) 
+    GetAlphabeticalCharacters(false).forEach(element => { validCharacters.push(element) });
+  if (ConfirmationMessage("numbers")) 
+    GetNumericalCharacters().forEach(element => { validCharacters.push(element) });
+  if (ConfirmationMessage("special characters"))
+    GetSpecialCharacters().forEach(element => { validCharacters.push(element) });
 
-  if (areLowerCaseValid) {for (const element of lowerCaseCharcters) validCharacters.push(element);}
-  if (areUpperCaseValid) {for (const element of upperCaseCharacters) validCharacters.push(element);}
-  if (areNumberValid) {for (const element of numberCharacters) validCharacters.push(element);}
-  if (areSpecialValid) {for (const element of specialCharacters) validCharacters.push(element);}
-
+  //Check if there are vaid characters, exit if there aren't
   if (validCharacters.length <= 0) { NoValidCharacters(); return null; }
   else
   {
+    //Pick random characters from the vaid charcters array and add them to the password
     var password = "";
-
-    for (let i = 0; i < passwordLength; i++)
-    {
-      password += GetCharacter(validCharacters);
-    }
-  
+    for (let i = 0; i < passwordLength; i++) { password += GetCharacter(validCharacters); }
     return password;
   }
+}
+
+function ConfirmationMessage(messageText)
+{
+  return confirm("Should " + messageText + " be allowed in the password?");
 }
 
 function GetCharacter(characters)
@@ -64,13 +66,18 @@ function GetSpecialCharacters()
 
 function NoValidCharacters()
 {
-  alert("You must enable at least one option");
+  alert("You must enable at least one option.");
+}
+
+function PasswordIsNotANumber()
+{
+  alert("All I asked is that you type in a number and you couldn't even do that?\nGet it together.");
 }
 
 function PasswordIsTheWrongSize(tooSmall)
 {
   var size = tooSmall ? "small" : "large";
-  alert("The number you have entered was too " + size + ".\nThe password should be within 8 and 128 characters.");
+  alert("The number you have entered was too " + size + ".\nThe password should be within " + minCharacters + " and " + maxCharacters + " characters.");
 }
 
 function WritePassword()
